501a7f0828403139659c9abe86b9ecb9a55f57ab
augrinn <augrinn@gmail.com>
2021-04-17 22:49:04 +0300

    step3

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python
+from ..cli import welcome_user
 
 def main():
     print('Welcome to the Brain Games!')
+    print('test')
+    welcome_user()
+    
 
 
 if __name__ == "__main__":
